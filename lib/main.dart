1ae16ed60235d5e1bcb330ec12d457db98a4e0ba
Kevin Khong <kevin79ers@gmail.com>
2023-04-27 22:01:26 -0700

    Initial commit, First Basic HomePage/Scaffold

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Carded',
+      theme: ThemeData(
+
+        primarySwatch: Colors.orange,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: CardedHomePage(),
+    );
+  }
+}
+
+
+class CardedHomePage extends StatelessWidget{
+  const CardedHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Carded")),
+        body: Column(children: <Widget>[
+          SizedBox(height: 200),
+          NavigationButton(),
+          SizedBox(height: 50),
+          (NavigationButton()),
+          SizedBox(height: 50),
+
+          NavigationButton()]),);
+
+  }
+
+}
+
+class NavigationButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    return Center(child: Text("Nav Button"));
+  }
+}
