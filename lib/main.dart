2ea1f8a9cde81f1b0648c9747a18735782b99b4b
Kevin Khong <kevin79ers@gmail.com>
2023-04-27 22:32:27 -0700

    main and widget file

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class CardedHomePage extends StatelessWidget{
         appBar: AppBar(title: Text("Carded")),
         body: Column(children: <Widget>[
           SizedBox(height: 200),
-          NavigationButton(),
+          TextInputWidget(),
           SizedBox(height: 50),
           (NavigationButton()),
           SizedBox(height: 50),
@@ -47,5 +47,19 @@ class NavigationButton extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Center(child: Text("Nav Button"));
+  }
+}
+
+class LoginWidget extends StatefulWidget {
+
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<LoginWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(decoration: InputDecoration(prefixIcon: Icon(Icons.account_circle), labelText: "Login Username:"),);
+
   }
 }
