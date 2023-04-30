b62f697a13815af78db7d6abdae36dae8db43d4d
Ayush <aynair123@gmail.com>
2023-04-30 16:50:13 -0700

    added new firebase dependencies add google signin option to signin page

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,26 +1,34 @@
 import 'package:flutter/material.dart';
 import 'username_text_field.dart';
 import 'password_text_field.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 class LoginScreen extends StatelessWidget {
-
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text("Login Screen")),
-      body: FractionallySizedBox(
-          heightFactor: 0.7,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-
-                UsernameTextField(),
-                PasswordTextField(),
-                ElevatedButton(onPressed: () {  },
-                    child: Text("Login!"))],
-            )
-        )
-
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: MaterialButton(
+          onPressed: () {
+            _googleSignIn.signIn().then((value) {
+              String username = value!.displayName!;
+              String profilePicture = value!.photoUrl!;
+            });
+          },
+          color: Colors.deepOrange,
+          height: 50,
+          minWidth: 100,
+          child: const Text(
+            'Google Sign in',
+            style: TextStyle(
+              color: Colors.white
+            ),
+          ),
+        ),
+      ),
     );
   }
 
--- lib/sign_up_screen.dart
+++ lib/sign_up_screen.dart
@@ -2,8 +2,9 @@ import 'package:carded/email_text_field.dart';
 import 'package:flutter/material.dart';
 import 'username_text_field.dart';
 import 'password_text_field.dart';
+import 'package:firebase_database/firebase_database.dart';
 class SignUpScreen extends StatelessWidget {
-
+  FirebaseDatabase database = FirebaseDatabase.instance;
   @override
   Widget build(BuildContext context){
     return Scaffold(
