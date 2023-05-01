227557b359330cad0cdd2971a8b329db2ef125c4
Kevin Khong <kevin79ers@gmail.com>
2023-04-30 23:39:26 -0700

    added hard coded settigns, readded settings page nav route

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:carded/settings_screen.dart';
 import 'package:carded/sign_up_screen.dart';
+import 'package:carded/wallet_display_screen.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'user.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -65,6 +69,35 @@ class CardedHomePage extends StatelessWidget{
                               MaterialPageRoute(builder: (context) => SignUpScreen()),
                             );
                           },
+                        ),
+                      ),
+                      SizedBox(height: 50),
+                      SizedBox(
+                        width: 200.0, // set the desired width here
+                        child: ElevatedButton(
+                          child: Text("Settings"),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => SettingsScreen()),
+                            );
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 50),
+
+                      //This is a test, hard coded wallet in here in case the sign in / login authroization does not work :D
+                      SizedBox(
+                        width: 200.0, // set the desired width here
+                        child: ElevatedButton(
+                          child: Text("wallet display"),
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => WalletDisplayScreen(loggedin: User("testID", "testEmail", "testCard", []))),
+                            );
+                          },
+
                         ),
                       ),
                     ]
