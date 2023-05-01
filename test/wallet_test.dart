1ad35df20a7ae4a056c8b3c61f56808393a45611
Castel Villalobos <54700341+impropernoun@users.noreply.github.com>
2023-04-30 23:30:41 -0700

    Update and rename wallet_tests.dart to wallet_test.dart

---
+++ test/wallet_test.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:carded/main.dart';
+import 'package:carded/login_screen.dart';
+
+void main() {
+
+  // testWidgets('Pressing the Show Wallet Button shows wallets', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //   await tester.tap(find.text("Wallet Page"));
+  //   await tester.pumpAndSettle();
+  //
+  //   await tester.tap(find.text("Show Wallet"));
+  //   await tester.pumpAndSettle();
+  //
+  //   expect(find.text("Hide Wallet"), findsOneWidget);
+  // });
+  //
+  // testWidgets('Pressing the Hide Wallet Button goes back to the previous page', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //   await tester.tap(find.text("Show Wallet"));
+  //   await tester.pumpAndSettle();
+  //
+  //   await tester.tap(find.text("Hide Wallet"));
+  //   await tester.pumpAndSettle();
+  //
+  //   expect(find.text("Show Wallet"), findsOneWidget);
+  // });
+}
