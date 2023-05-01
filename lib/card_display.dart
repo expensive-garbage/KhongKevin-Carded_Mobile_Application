d85a91bde190971ed1afef8196ff467b282198dd
Kevin Khong <kevin79ers@gmail.com>
2023-04-30 19:02:53 -0700

    added pfp

--- lib/card_display.dart
+++ lib/card_display.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
-import 'user_card.dart';
+import 'dart:ui'; // Import dart:ui library to use Image.asset
 
 class CardDisplay extends StatelessWidget {
   final String firstName;
   final String lastName;
   final String email;
+  final String? profilePictureUrl;
   final String? linkedin;
   final String? website;
+
+  final String defaultProfilePicture = 'assets/images/profile.png'; // Declare the asset path for the default profile picture
 
   CardDisplay({
     required this.firstName,
     required this.lastName,
     required this.email,
+    this.profilePictureUrl,
     this.linkedin,
     this.website,
   });
@@ -19,6 +23,7 @@ class CardDisplay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.orangeAccent,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
@@ -27,12 +32,23 @@ class CardDisplay extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Text(
-              '$firstName $lastName',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold,
-              ),
+            Row(
+              children: [
+                CircleAvatar(
+                  radius: 30,
+                  backgroundImage: profilePictureUrl != null
+                      ? NetworkImage(profilePictureUrl!)
+                      : AssetImage(defaultProfilePicture) as ImageProvider,
+                ),
+                SizedBox(width: 16),
+                Text(
+                  '$firstName $lastName',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
             ),
             SizedBox(height: 8.0),
             Text(
