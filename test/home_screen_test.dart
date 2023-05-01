01b9fbaf6dae08082b2b70719dc3b294e115b96a
Castel Villalobos <54700341+impropernoun@users.noreply.github.com>
2023-04-30 23:30:25 -0700

    Update and rename home_screen_tests.dart to home_screen_test.dart

---
+++ test/home_screen_test.dart
@@ -0,0 +1,44 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/button_view.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:carded/main.dart';
+import 'package:carded/login_screen.dart';
+
+void main() {
+
+  testWidgets('Pressing the Login Page button navigates to the Login Screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text("Login Page"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("Google Sign in"), findsOneWidget);
+
+  });
+
+  // testWidgets('Pressing the Sign Up Page button navigates to the Sign Up Screen', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //
+  //   await tester.tap(find.text("Sign Up Page"));
+  //   await tester.pumpAndSettle();
+  //
+  //   // what am i doing
+  //   expect(find.text("Sign in with Google"), findsNothing);
+  // });
+
+  // testWidgets('Pressing the Wallet Page button navigates to the Wallet Screen', (WidgetTester tester) async {
+  //   await tester.pumpWidget(const MyApp());
+  //
+  //   await tester.tap(find.text("Wallet Page"));
+  //   await tester.pumpAndSettle();
+  //
+  //   expect(find.text("Show Wallet"), findsOneWidget);
+  // });
+}
