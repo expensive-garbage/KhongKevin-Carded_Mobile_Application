2cc31604aef5e962d69920e062d65c62a820a527
Castel Villalobos <54700341+impropernoun@users.noreply.github.com>
2023-04-30 23:55:04 -0700

    Update home_screen_test.dart

--- test/home_screen_test.dart
+++ test/home_screen_test.dart
@@ -18,25 +18,25 @@ void main() {
     await tester.tap(find.text("Login Page"));
     await tester.pumpAndSettle();
 
-    expect(find.text("Login Screen"), findsOneWidget);
+    expect(find.text("Google Sign in"), findsOneWidget);
 
-  });
-
-  testWidgets('Pressing the Sign Up Page button navigates to the Sign Up Screen', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-
-    await tester.tap(find.text("Sign Up Page"));
-    await tester.pumpAndSettle();
-
-    expect(find.text("Sign Up Screen"), findsOneWidget);
   });
 
   testWidgets('Pressing the Settings Page button navigates to the Settings Screen', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    await tester.tap(find.text("Settings Page"));
+    await tester.tap(find.text("Settings"));
     await tester.pumpAndSettle();
 
     expect(find.text("I am broke"), findsOneWidget);
+  });
+
+  testWidgets('Pressing the wallet display button navigates to the wallet Screen', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text("wallet display"));
+    await tester.pumpAndSettle();
+
+    expect(find.text("Show Wallet"), findsOneWidget);
   });
 }
