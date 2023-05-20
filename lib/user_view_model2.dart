d441341068c218db3e8ff017399d51c892220313
Kevin Khong <kevin79ers@gmail.com>
2023-05-19 22:57:28 -0700

    Refactoring into proper MVVM format

---
+++ lib/user_view_model2.dart
@@ -0,0 +1,37 @@
+import 'package:carded/user_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/*refactor into mvvm*/
+
+FirebaseFirestore database = FirebaseFirestore.instance;
+
+class User{
+  late String refId;
+  late String email;
+  late String card;
+  List<String> wallet = [];
+
+  User(String r, String e, String c, List<dynamic> w){
+    refId = r;
+    email = e;
+    card = c;
+    wallet = List<String>.from(w);
+  }
+
+  static Future<void> addUser(String email, String fname, String lname) async {
+    String cardID = await User_Card.addCard(fname, lname, email);
+    print("Card added");
+    final user = <String, dynamic>{
+      "Card": cardID,
+      "Email": email,
+      "Wallet":[]
+    };
+    database.collection("users").add(user);
+    print("User added");
+  }
+
+  @override
+  String toString(){
+    return("${refId}, ${email}, ${card}, ${wallet}");
+  }
+}
