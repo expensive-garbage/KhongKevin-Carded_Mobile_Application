6381af8ddef9c90d874c8078a0eb012570439dcc
Kevin Khong <kevin79ers@gmail.com>
2023-05-19 22:57:28 -0700

    Refactoring into proper MVVM format

---
+++ lib/user_view_model.dart
@@ -0,0 +1,18 @@
+class UserModel {
+  late String refId;
+  late String email;
+  late String card;
+  List<String> wallet = [];
+
+  UserModel(String r, String e, String c, List<dynamic> w){
+    refId = r;
+    email = e;
+    card = c;
+    wallet = List<String>.from(w);
+  }
+
+  @override
+  String toString(){
+    return("${refId}, ${email}, ${card}, ${wallet}");
+  }
+}
