3dec0320983aebda97594e8c6b72709a790b14be
Castel Villalobos <54700341+impropernoun@users.noreply.github.com>
2023-05-21 02:42:22 -0700

    fixed some tests

--- lib/main.dart
+++ lib/main.dart
@@ -41,96 +41,97 @@ class CardedHomePage extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Carded")),
-        body: Container(
-            child: FractionallySizedBox(
-              heightFactor: 0.7,
-              child:
-              Center(
-                child:Column(
-                    mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      SizedBox(width: 200.0, child: ElevatedButton(
-                          child: Text("Login Page"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => LoginScreen()),
-                            );
-                          },
-                        ),
-                      ),
+        body: FractionallySizedBox(
+          heightFactor: 0.7,
+          child:
+          Center(
+            child:
+            SingleChildScrollView(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center, crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  SizedBox(width: 200.0, child: ElevatedButton(
+                      child: Text("Login Page"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => LoginScreen()),
+                        );
+                      },
+                    ),
+                  ),
 
-                      SizedBox(height: 50),
-                      SizedBox(
-                        width: 200.0, // set the desired width here
-                        child: ElevatedButton(
-                          child: Text("Sign Up Page"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SignUpScreen()),
-                            );
-                          },
-                        ),
-                      ),
-                      SizedBox(height: 50),
-                      SizedBox(
-                        width: 200.0, // set the desired width here
-                        child: ElevatedButton(
-                          child: Text("Settings"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SettingsScreen()),
-                            );
-                          },
-                        ),
-                      ),
-                      SizedBox(height: 50),
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: 200.0, // set the desired width here
+                    child: ElevatedButton(
+                      child: Text("Sign Up Page"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignUpScreen()),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  SizedBox(
+                    width: 200.0, // set the desired width here
+                    child: ElevatedButton(
+                      child: Text("Settings"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SettingsScreen()),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 50),
 
-                      //This is a test, hard coded wallet in here in case the sign in / login authroization does not work :D
-                      SizedBox(
-                        width: 200.0, // set the desired width here
-                        child: ElevatedButton(
-                          child: Text("wallet display"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => WalletDisplayScreen(loggedin: User("testID", "testEmail", "testCard", []))),
-                            );
-                          },
+                  //This is a test, hard coded wallet in here in case the sign in / login authroization does not work :D
+                  SizedBox(
+                    width: 200.0, // set the desired width here
+                    child: ElevatedButton(
+                      child: Text("wallet display"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => WalletDisplayScreen(loggedin: User("testID", "testEmail", "testCard", []))),
+                        );
+                      },
 
-                        ),
-                      ),
-                      SizedBox(
-                        width: 200.0, // set the desired width here
-                        child: ElevatedButton(
-                          child: Text("QR Generator"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => QRCodePage(loggedIn: User("testID", "testEmail", "testCard", []))),
-                            );
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        width: 200.0, // set the desired width here
-                        child: ElevatedButton(
-                          child: Text("QR Scanner"),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => QRScannerPage()),
-                            );
-                          },
-                        ),
-                      ),
-                    ]
-                ),
-              )
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200.0, // set the desired width here
+                    child: ElevatedButton(
+                      child: Text("QR Generator"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => QRCodePage(loggedIn: User("testID", "testEmail", "testCard", []))),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 200.0, // set the desired width here
+                    child: ElevatedButton(
+                      child: Text("QR Scanner"),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => QRScannerPage()),
+                        );
+                      },
+                    ),
+                  ),
+                ]
+            )
             )
           )
+        )
         );
 
   }
